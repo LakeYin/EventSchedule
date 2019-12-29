8f4c9a5aa7aafe30b43bed3f82ccf343c6e95b8a
Lake Yin <la11010e@gmail.com>
2019-12-29 16:37:09 -0500

    Setting up files

---
+++ src/BotScheduler.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+public class BotScheduler implements EventListener
+{
+	@Override
+	public void onEvent(GenericEvent event) 
+	{
+		// TODO: add scheduling
+		
+	}
+
+}
---
+++ src/EventSchedule.java
@@ -0,0 +1,9 @@
+import javax.security.auth.login.LoginException;
+
+public class EventSchedule 
+{
+	public static void main(String[] args) throws LoginException, InterruptedException
+	{
+		
+	}
+}
